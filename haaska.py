cb00ee2c55b7770970e353a6fe0228e42e95e6ef
lucas_nz <606314+lucasnz@users.noreply.github.com>
2024-12-31 18:20:20 +1300

    Fix get_url

--- haaska.py
+++ haaska.py
@@ -94,12 +94,19 @@ class Configuration(object):
                 return self._json[key]
         return default
 
+    def get_base_url(url):
+        if "://" in url:
+            scheme, rest = url.split("://", 1)
+            netloc = rest.split("/", 1)[0]
+            return f"{scheme}://{netloc}"
+        return url
+    
     def get_url(self, url):
         """Returns Home Assistant base url without '/api' or trailing slash"""
         if not url:
             raise ValueError('Property "url" is missing in config')
 
-        return url.removesuffix("/api").removesuffix("/")
+        return get_base_url(url)
 
 
 def event_handler(event, context):
